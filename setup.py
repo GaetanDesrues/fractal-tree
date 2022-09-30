30f4099cb54f55c487fe33deab5a58aa543eeb8b
Gaetan Desrues <gaetan.desrues@inria.fr>
2022-09-30 16:16:43 +0200

    new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="FractalTree",
-    version="0.0.61",
+    version="0.0.62",
     url="https://github.com/GaetanDesrues/fractal-tree",
     packages=["FractalTree"],
     include_package_data=True,
